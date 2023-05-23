3c984c8d3684a800b36f4d745543f42228cf1c8e
Leocarnauba <leocarmacedo@hotmail.com>
2023-05-23 19:07:16 -0300

    ClientDTO Validations

--- src/main/java/com/carnacorp/desafio3/dto/ClientDTO.java
+++ src/main/java/com/carnacorp/desafio3/dto/ClientDTO.java
@@ -4,13 +4,37 @@ import java.time.LocalDate;
 
 import com.carnacorp.desafio3.entities.Client;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.PastOrPresent;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.PositiveOrZero;
+import jakarta.validation.constraints.Size;
+
 public class ClientDTO {
 
 	private Long id;
+	
+	@Size(min = 3, max = 80, message = "Nome precisa ter entre 3 e 80 caracteres")
+	@NotBlank(message = "Campo requerido")
 	private String name;
+	
+	@Size(min = 11, max = 11, message = "O CPF deve conter 11 dígitos")
+	@Pattern(regexp = "\\d+", message = "O CPF deve conter apenas números")
+	@NotBlank(message = "Campo requerido")
 	private String cpf;
+	
+	@Positive(message = "O salário deve ter um valor positivo")
+	@NotBlank(message = "Campo requerido")
 	private Double income;
+	
+	@PastOrPresent(message = "A data de nascimento não pode ser futura")
+	@NotBlank(message = "Campo requerido")
+	@Pattern(regexp = "\\d{4}-\\d{2}-\\d{2}", message = "A data deve estar no formato yyyy-MM-dd")
 	private LocalDate birthDate;
+	
+	@PositiveOrZero(message = "O valor deve ser positivo ou zero")
+	@NotBlank(message = "Campo requerido")
 	private Integer children;
 
 	public ClientDTO(Long id, String name, String cpf, Double income, LocalDate birthDate, Integer children) {
